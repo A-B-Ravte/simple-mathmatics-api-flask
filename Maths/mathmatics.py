272c08b3fb1e320cdfede3956a9c2b6dc45fb468
A-B-Ravte <letsmailravte@gmail.com>
2026-01-19 18:52:25 +0530

    Created package and merge code with server.py

---
+++ Maths/mathmatics.py
@@ -0,0 +1,16 @@
+def summation(a:float, b:float) :
+    result = a + b
+    return result 
+
+def subtraction(a:float, b:float) :
+    result = a - b
+    return result 
+
+def multiplication(a:float, b:float) :
+    result = a * b
+    return result
+
+def division(a:float, b:float) :
+    result = a / b
+    return result  
+
